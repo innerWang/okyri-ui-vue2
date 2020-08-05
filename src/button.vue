0f4ba1cd7953fd262109fb7a8b889996fd95b9c8
innerwang <52886377@qq.com>
2020-08-05 18:02:01 +0800

    feat: set icon & iconPosition for button

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,35 @@
 <template>
-  <button class="okyri-button">按钮</button>
+  <button class="okyri-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function(value) {
+        return ['left', 'right'].indexOf(value) !== -1;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 .okyri-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle; // 内联元素不对齐使用 vertical-align
   height: var(--button-height);
   font-size: var(--font-size);
   padding: 0 1em;
@@ -25,6 +47,25 @@ export default {};
 
   &:focus {
     outline: none;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: 0.2em;
+  }
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: 0.2em;
+      margin-right: 0;
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
