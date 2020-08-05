0f3403045e074e608267ec9d66b81aa6e4f56486
innerwang <52886377@qq.com>
2020-08-05 18:12:56 +0800

    feat: add icon component

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
   <button class="okyri-button" :class="{ [`icon-${iconPosition}`]: true }">
-    <svg v-if="icon" class="icon">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
+    <okyri-icon class="icon" v-if="icon" :name="icon"></okyri-icon>
     <div class="content">
       <slot></slot>
     </div>
---
+++ src/icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="okyri-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.okyri-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
