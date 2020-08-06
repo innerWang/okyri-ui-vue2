354eb3d567d79575cb6b12c25eb694770989c7a3
innerwang <52886377@qq.com>
2020-08-06 14:25:35 +0800

    chore: change git config[core.ignorecase] and update

---
+++ src/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="o-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.o-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
