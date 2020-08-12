083c189c63b0d75b866812edb9c938e1a8bba375
innerwang <52886377@qq.com>
2020-08-12 17:20:38 +0800

    feat: add Layout

---
+++ src/Layout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="layout" :class="layoutClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OkyriLayout',
+  data: function () {
+    return {
+      layoutClass: {
+        hasSider: false,
+      },
+    };
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      // name 属性的第二个用法，可以找到子元素中是否包含特定的组件
+      if (vm.$options.name === 'OkyriSider') {
+        this.layoutClass.hasSider = true;
+      }
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+
+  &.hasSider {
+    flex-direction: row;
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './ButtonGroup.vue';
 import Input from './Input';
 import Row from './Row';
 import Col from './Col';
+import Layout from './Layout';
+import Header from './Header';
+import Footer from './Footer';
+import Sider from './Sider';
+import Content from './Content';
 
 Vue.component('o-button', Button);
 Vue.component('o-icon', Icon);
@@ -12,6 +17,11 @@ Vue.component('o-button-group', ButtonGroup);
 Vue.component('o-input', Input);
 Vue.component('o-row', Row);
 Vue.component('o-col', Col);
+Vue.component('o-layout', Layout);
+Vue.component('o-header', Header);
+Vue.component('o-footer', Footer);
+Vue.component('o-content', Content);
+Vue.component('o-sider', Sider);
 
 new Vue({
   el: '#app',
