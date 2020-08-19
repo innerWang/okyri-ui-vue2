2f30c215b52288b1bdbe99150a59f22384fbf4c9
innerwang <52886377@qq.com>
2020-08-19 09:48:37 +0800

    [popover]: move content to body

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      v-if="visible"
+      @click.stop
+    >
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper" class="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -20,6 +27,17 @@ export default {
       this.visible = !this.visible;
       if (this.visible) {
         this.$nextTick(() => {
+          // 确保渲染后，将其移到body里面
+          document.body.appendChild(this.$refs.contentWrapper);
+          const {
+            left,
+            top,
+            width,
+            height,
+          } = this.$refs.triggerWrapper.getBoundingClientRect();
+
+          this.$refs.contentWrapper.style.left = `${left}px`;
+          this.$refs.contentWrapper.style.top = `${top}px`;
           const clickEventHandle = () => {
             this.visible = false;
             document.removeEventListener('click', clickEventHandle);
@@ -36,14 +54,15 @@ export default {
 .popover {
   display: inline-block;
   vertical-align: top;
-  position: relative;
 
-  .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid #666;
-    box-shadow: 0 0 3px rgba($color: #000000, $alpha: 0.5);
+  .triggerWrapper {
+    display: inline-block;
   }
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px solid #666;
+  box-shadow: 0 0 3px rgba($color: #000000, $alpha: 0.5);
+  transform: translateY(-100%);
 }
 </style>
